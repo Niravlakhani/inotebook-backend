1fd3fe700280e25992633a4b9375b99ceddee865
Niravlakhani <niravlakhani020@gmail.com>
2023-08-21 19:08:22 +0530

    create a new end points for update notes and delete notes

--- routes/notes.js
+++ routes/notes.js
@@ -41,12 +41,93 @@ router.post(
       });
 
       const saveNote = await note.save();
-      res.send(saveNote);
+      res.send({ saveNote, message: "Note has been created successfully!" });
     } catch (error) {
       console.error(error);
       return res.status(500).send({ error: "Internal Server Error" });
     }
   }
 );
+
+// ROUTE 3: Update The existing Note Using : Put "/api/notes/update-note/:id" Login required
+
+router.put(
+  "/update-note/:id",
+  useMiddleware,
+  [
+    body("title", "Title must be atleast 5 character").isLength({ min: 3 }),
+    body("description", "description must be atleast 5 character").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    const { title, description, tag } = req.body;
+    const { id } = req.params;
+
+    try {
+      const newNoteData = {};
+
+      // create a new object
+      if (title) {
+        newNoteData.title = title;
+      }
+      if (description) {
+        newNoteData.description = description;
+      }
+      if (tag) {
+        newNoteData.tag = tag;
+      }
+
+      // find the note to be updated and update it.
+
+      const note = await Notes.findById(id);
+      if (!note) {
+        return res.status(404).send("Not Found");
+      }
+
+      if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Unauthorised user!");
+      }
+
+      const result = await Notes.findByIdAndUpdate(
+        id,
+        { $set: newNoteData },
+        { new: true }
+      );
+
+      res.json({ result, message: "Note has been updated successfully!" });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send({ error: "Internal Server Error" });
+    }
+  }
+);
+
+// ROUTE 4: Delete The existing Note Using : Delete "/api/notes/delete-note/:id" Login required
+
+router.delete("/delete-note/:id", useMiddleware, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    // find the note to be updated and delete it.
+
+    const note = await Notes.findById(id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+
+    // allow deletion only if user owns this note
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Unauthorised user!");
+    }
+    const result = await Notes.findByIdAndDelete(id);
+    if (result) {
+      res.json({ message: "Note has been deleted successfully!" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
